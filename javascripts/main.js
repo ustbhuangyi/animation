45ece6f54ecc79f4f5309edd8a0173ba267de893
ustbhuangyi <280309453@qq.com>
2014-02-12 17:11:27 +0800

    bug fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,7 +18,7 @@ define(function (require) {
         rabbitWinMap = ["0 0", "-198 0", "-401 0", "-609 0", "-816 0", "0 -96", "-208 -97", "-415 -97", "-623 -97", "-831 -97", "0 -203", "-207 -203", "-415 -203", "-623 -203", "-831 -203", "0 -307", "-206 -307", "-414 -307", "-623 -307"],
         rabbitLoseMap = ["0 0", "-163 0", "-327 0", "-491 0", "-655 0", "-819 0", "0 -135", "-166 -135", "-333 -135", "-500 -135", "-668 -135", "-835 -135", "0 -262"];
 
-    repeatAnimation = animation().changePosition($rabbit1, runningMap).repeat();
+    repeatAnimation = animation().changePosition($rabbit1, rightRunningMap).repeat();
 
     function $(id) {
         return document.getElementById(id);
