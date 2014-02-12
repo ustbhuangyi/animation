5f8e812d5b8e83ad26d7b27a061cf1fc893145fe
ustbhuangyi <280309453@qq.com>
2014-02-12 17:31:11 +0800

    demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,17 +8,61 @@ define(function (require) {
         $rabbit3 = $('rabbit3'),
         $rabbit4 = $('rabbit4');
 
-    var repeatAnimation,
-        runAnimation,
-        winAnimation,
-        loseAnimation;
-
-    var rightRunningMap = ["0 -854", "-174 -852", "-349 -852", "-524 -852", "-698 -851", "-873 -848"],  //Õ√◊””“≈‹
+    var rightRunningMap = ["0 -854", "-174 -852", "-349 -852", "-524 -852", "-698 -851", "-873 -848"],
         leftRunningMap = ["-2px 0", "-176px -2px", "-351px -3px", "-524px -3px", "-698px -3px", "-874px -3px"],
         rabbitWinMap = ["0 0", "-198 0", "-401 0", "-609 0", "-816 0", "0 -96", "-208 -97", "-415 -97", "-623 -97", "-831 -97", "0 -203", "-207 -203", "-415 -203", "-623 -203", "-831 -203", "0 -307", "-206 -307", "-414 -307", "-623 -307"],
         rabbitLoseMap = ["0 0", "-163 0", "-327 0", "-491 0", "-655 0", "-819 0", "0 -135", "-166 -135", "-333 -135", "-500 -135", "-668 -135", "-835 -135", "0 -262"];
 
-    repeatAnimation = animation().changePosition($rabbit1, rightRunningMap).repeat();
+    function repeat() {
+        var repeatAnimation = animation().changePosition($rabbit1, rightRunningMap).repeat();
+        repeatAnimation.start(80);
+
+    }
+
+    function run() {
+        var interval = 50,
+            speed = 6,
+            left, position,
+            initLeft = 100,
+            finalLeft = 500,
+            frame = 4,
+            frameLength = 6,
+            right = true;
+
+        var runAnimation = animation().enterframe(function (success, time) {
+            var ratio = (time) / interval;
+            if (right) {
+                position = rightRunningMap[frame].split(" ");
+                left = Math.min(initLeft + speed * ratio, finalLeft);
+                if (left == finalLeft) {
+                    right = false;
+                    frame = 4;
+                }
+            } else {
+                position = leftRunningMap[frame].split(" ");
+                left = Math.max(finalLeft - speed * ratio, initLeft);
+                if (left == initLeft) {
+                    right = true;
+                    frame = 4;
+                }
+            }
+            if (++frame == RUN_FRAME_LENGTH) {
+                frame = 0;
+            }
+            $rabbit2.style.backgroundPosition = position[0] + "px " + position[1] + "px";
+            $rabbit2.style.left = left;
+        }).repeat();
+        runAnimation.start(interval);
+    }
+
+    function win() {
+        var winAnimation;
+
+    }
+
+    function lose() {
+        var loseAnimation;
+    }
 
     function $(id) {
         return document.getElementById(id);
