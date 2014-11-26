0b54f7e5d1dd4f93b4f76599dffb92044326d8b7
ustbhuangyi <280309453@qq.com>
2014-11-26 18:04:32 +0800

    测试delay

--- javascripts/main.js
+++ javascripts/main.js
@@ -58,7 +58,7 @@ define(function (require) {
             }
             $rabbit2.style.backgroundPosition = position[0] + "px " + position[1] + "px";
             $rabbit2.style.left = left + "px";
-        }).repeat(6, {delay: 1000}).delay(3000).changePosition($rabbit3, rabbitWinMap).repeat(3, {delay: 2000}).delay(3000).changePosition($rabbit4, rabbitLoseMap).delay(3000).then(function () {
+        }).repeat(6, {delay: 1000}).delay(6000).changePosition($rabbit3, rabbitWinMap).repeat(3, {delay: 2000}).delay(3000).changePosition($rabbit4, rabbitLoseMap).delay(3000).then(function () {
             console.log("finish");
         });
         runAnimation.start(interval);
